99a8fcc8a71c61621d3b6a51e68a75f70085e5a3
pood0g <>
2023-04-29 17:09:01 +1000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module socker
+
+go 1.18
+
+require github.com/things-go/go-socks5 v0.0.3
+
+require github.com/akamensky/argparse v1.4.0
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/akamensky/argparse"
+	"github.com/things-go/go-socks5"
+)
+
+func main() {
+
+	parser := argparse.NewParser("Socker", "Creates a SOCKS5 proxy")
+
+	iface := parser.String("i", "interface",
+		&argparse.Options{
+			Required: false,
+			Help:     "The interface to listen on.",
+			Default:  "127.0.0.1",
+		})
+
+	port := parser.String("p", "port",
+		&argparse.Options{
+			Required: false,
+			Help:     "The port to listen on",
+			Default:  "1080",
+		})
+
+	username := parser.String("U", "user",
+		&argparse.Options{
+			Required: false,
+			Help:     "Username to use for proxy",
+			Default:  "socker",
+		})
+
+	password := parser.String("P", "pass",
+		&argparse.Options{
+			Required: false,
+			Help:     "Password to use for proxy",
+		})
+
+	argErr := parser.Parse(os.Args)
+
+	if argErr != nil {
+		fmt.Print(parser.Usage(argErr))
+		os.Exit(1)
+	}
+
+	// set the listener string
+	var listnr = fmt.Sprintf("%s:%s", *iface, *port)
+	fmt.Printf("Starting SOCKS5 Proxy Listening on %s\n\n", listnr)
+
+	// set options for the server
+	opt := []socks5.Option{
+		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
+		nil,
+	}
+
+	opt = opt[:1]
+
+	// Create a SOCKS5 server
+	if len(*password) > 0 {
+		fmt.Printf("Auth Mode enabled...\n\tUsername: %s\n\n", *username)
+		creds := socks5.StaticCredentials{*username: *password}
+		auth := socks5.UserPassAuthenticator{Credentials: creds}
+		opt = opt[:2]
+		opt[1] = socks5.WithAuthMethods([]socks5.Authenticator{auth})
+	}
+
+	server := socks5.NewServer(opt...)
+
+	// Serve SOCKS5 Proxy
+	if err := server.ListenAndServe("tcp", listnr); err != nil {
+		panic(err)
+	}
+}
